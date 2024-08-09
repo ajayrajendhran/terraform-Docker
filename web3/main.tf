03f204a2a3e1286f638cb38e1069c01e8fe2a668
ajay <ajaykavitha213@gmail.com>
2024-08-09 13:17:01 +0530

    portchange  commit

--- web3/main.tf
+++ web3/main.tf
@@ -24,10 +24,10 @@ resource "docker_container" "web_container" {
 
   ports {
     internal = 80
-    external = 8070
+    external = 8090
   }
 }
 
 output "website_url" {
-  value = "http://localhost:8070"
+  value = "http://localhost:8090"
 }
